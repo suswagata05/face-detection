346fe3b9d6d026b29610cc5308e5edc0f411a22a
suswagata05 <suswagata05@gmail.com>
2024-12-03 20:30:07 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -5,7 +5,7 @@ mp_face_detection = mp.solutions.face_detection
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
-# Start the camera
+# Start the cam
 print("To start the camera, type 'start' and press enter:")
 while True:
   user_input = input()
@@ -14,7 +14,7 @@ while True:
   else:
     print("Invalid input. Please type 'start' to begin.")
 
-cap = cv2.VideoCapture(0)  # Use 0 for default camera
+cap = cv2.VideoCapture(0)  
 
 with mp_face_detection.FaceDetection(
     model_selection=1, min_detection_confidence=0.5) as face_detection, \
@@ -25,14 +25,13 @@ with mp_face_detection.FaceDetection(
       print("Ignoring empty camera frame.")
       continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
+    # To improve performance, optionally mark the image as not writeable
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results_face = face_detection.process(image)
     results_hands = hands.process(image)
 
-    # Draw the face detection annotations on the image.
+    # Draw the face detection annotations 
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results_face.detections:
@@ -47,9 +46,9 @@ with mp_face_detection.FaceDetection(
             mp_drawing.DrawingSpec(color=(153, 51, 255), thickness=1),
             mp_drawing.DrawingSpec(color=(102, 178, 255), thickness=2))
 
-    # Flip the image horizontally for a selfie-view display.
+    # Flip the image horizontally for a selfie-view 
     cv2.imshow('image',cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == ord('a'): #a=97,ASCII Value of a
+    if cv2.waitKey(5) & 0xFF == ord('x'):
       break
     
 cap.release()
